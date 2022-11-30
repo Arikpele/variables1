2d8a2cbc39e9891f7c6bb1595b46a7696d3823bb
Arikpele <79112835522@y.ru>
2022-11-30 11:31:15 +0300

    задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 10;
-        var cat = 3.5;
-        var paper = 763789;
-        var mouse = dog+4;
-        var man=cat+4;
-        var woman=paper+4;
-        var x=dog-3.5;
-        var z=cat-1.6;
-        var c= paper-7639;
-        System.out.println(x);
-        System.out.println(z);
-        System.out.println(c);
+        var frog=19;
+        System.out.println(frog);
+        frog=19*2;
+        System.out.println(frog);
+        frog=frog/7;
+        System.out.println(frog);
     }
 }
