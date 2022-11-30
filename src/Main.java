58bd0c4769f720f31d97bdd160f2f3be48ec1ccf
Arikpele <79112835522@y.ru>
2022-11-30 11:33:25 +0300

    задание 7

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var firstBoxer=78.2;
         var secondBoxer=82.7;
-        System.out.println("Общий вес бойцов"+(firstBoxer+secondBoxer));
-        System.out.println("Разница в весах"+(secondBoxer-firstBoxer));
+        var weightDifference=secondBoxer-firstBoxer;
+        System.out.println("разница в весе"+weightDifference+"кг");
+        var over = (secondBoxer+firstBoxer)%weightDifference;
+        System.out.println (over);
     }
 }
