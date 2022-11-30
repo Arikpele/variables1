4f1a0e5dc9d4ebc685a00c26dcff957aa0e4ea2e
Arikpele <79112835522@y.ru>
2022-11-30 11:25:52 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println (dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var man = 8;
+        var woman = 3.6;
+        var mouse = 763789;
+        man=man+4;
+        woman=woman+4;
+        mouse=mouse+4;
+        System.out.println (man);
+        System.out.println(woman);
+        System.out.println(mouse);
     }
 }
